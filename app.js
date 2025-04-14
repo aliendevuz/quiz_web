9160feb1f46462ef4c400eec8e74cf7a21df96d3
aliendevuz <aliendevuz@gmail.com>
2025-04-14 09:56:51 +0500

    for get url

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 
 
 
+const en = "https://raw.githubusercontent.com/aliendevuz/quiz_web/refs/heads/master/strings/en.json";
+const uz = "https://raw.githubusercontent.com/aliendevuz/quiz_web/refs/heads/master/strings/uz.json";
+
+fetch(en)
+.then(response => response.json())
+.then(response => {
+    console.log(response);
+})
